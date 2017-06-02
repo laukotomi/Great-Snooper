8ea0b0c7f37f79f3bdb18327bc936a0a97c8b7f1
Tamás Laukó <laukotomi@gmail.com>
2017-06-02 11:21:13 +0200

    Added Bogus faker and TestCommand

---
+++ Great Snooper/UserCommands/TestCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Bogus;
+using GreatSnooper.Helpers;
+using GreatSnooper.IRCTasks;
+
+namespace GreatSnooper.UserCommands
+{
+    class TestCommand : UserCommand
+    {
+        private static Random rnd = new Random();
+
+        public TestCommand()
+            : base("test")
+        {
+
+        }
+
+        public override void Run(ViewModel.AbstractChannelViewModel sender, string command, string text)
+        {
+            int number;
+            if (int.TryParse(text, out number))
+            {
+                var faker = new Faker<MessageTask>()
+                    .CustomInstantiator(f =>
+                    {
+                        int index = rnd.Next(sender.Users.Count);
+                        return new MessageTask(sender.Server, sender.Users[index].Name, sender.Name, f.Lorem.Sentence(), MessageSettings.ChannelMessage);
+                    });
+
+                foreach (MessageTask task in faker.Generate(number))
+                {
+                    sender.MainViewModel.HandleTask(task);
+                }
+            }
+        }
+    }
+}
